a639a4b6ee503376d96a9c8b889e24fb8b07e45a
Shreya Malviya <malviyashreya26@gmail.com>
2021-05-28 11:16:36 +0530

    [Shreya]Add: Calculate Harmonic Number Series

--- Basic_Core_Program/Basic_Core_Program/Program.cs
+++ Basic_Core_Program/Basic_Core_Program/Program.cs
@@ -16,6 +16,7 @@ namespace Basic_Core_Program
             Console.WriteLine("1 For: Flip a Coin Head and Tail. ");
             Console.WriteLine("2 For: Select Value is Leap Year Or not");
             Console.WriteLine("3 For: Select Power of 2 Number");
+            Console.WriteLine("4 For: Select the Harmonic Number");
             int selection = Convert.ToInt32(Console.ReadLine());
            
             // user selection
@@ -33,8 +34,14 @@ namespace Basic_Core_Program
                     LeapYearOrNot.LeapYear();
                     break;
                 case 3:
+                    //Power Of 2
                     CalculatePowerOf2 CalPower = new CalculatePowerOf2();
                     CalPower.CalcuOfPower();
+                    break;
+                case 4:
+                    //Harmonic Series
+                    HarmonicNumber CalculateHarmonicNumber = new HarmonicNumber();
+                    CalculateHarmonicNumber.CalcHarmonicSeries();
                     break;
                 default:
                     Console.WriteLine("Invalid Selection. Select inBetween 1 to 2");
