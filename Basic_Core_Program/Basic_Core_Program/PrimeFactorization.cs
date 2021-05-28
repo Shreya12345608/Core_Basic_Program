ce7ed25f242fbc0aa95eadf041697f3ef699df00
Shreya Malviya <malviyashreya26@gmail.com>
2021-05-28 11:24:16 +0530

    [Shreya]Add: Calculate Prime Factor

---
+++ Basic_Core_Program/Basic_Core_Program/PrimeFactorization.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core_Program
+{
+    class PrimeFactorization
+    {
+        //define variable
+        public int Var_j = 0;
+        public int num = 0;
+        public int factorial = 1;
+        public void CalcSFactorialSeries()
+        {
+            Console.Write("Calculate the factorial of a given number:"); //Input From User
+            num = Convert.ToInt32(Console.ReadLine());//reading input form user
+            for (Var_j = 1; Var_j <= num; Var_j++) //Checking var_j is <= num example(1<5) and den incrementing it by 1 
+                factorial *= Var_j; //ca;lculate  factorial = factorial * var_j example (factroial = 1*1 or 2*2 or 5*5)
+            Console.Write("The Factorial of {0} is: {1}\n", num, factorial); //op
+        }
+    }
+}
--- Basic_Core_Program/Basic_Core_Program/Program.cs
+++ Basic_Core_Program/Basic_Core_Program/Program.cs
@@ -17,6 +17,7 @@ namespace Basic_Core_Program
             Console.WriteLine("2 For: Select Value is Leap Year Or not");
             Console.WriteLine("3 For: Select Power of 2 Number");
             Console.WriteLine("4 For: Select the Harmonic Number");
+            Console.WriteLine("5 For: Select Factors of number");
             int selection = Convert.ToInt32(Console.ReadLine());
            
             // user selection
@@ -42,6 +43,11 @@ namespace Basic_Core_Program
                     //Harmonic Series
                     HarmonicNumber CalculateHarmonicNumber = new HarmonicNumber();
                     CalculateHarmonicNumber.CalcHarmonicSeries();
+                    break;
+                case 5:
+                    //Prime Factor
+                    PrimeFactorization PrimeFactor = new PrimeFactorization();
+                    PrimeFactor.CalcSFactorialSeries();
                     break;
                 default:
                     Console.WriteLine("Invalid Selection. Select inBetween 1 to 2");
