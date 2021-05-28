970a6335702b77a2a5c9f737b07c0db7f5de6aea
Shreya Malviya <malviyashreya26@gmail.com>
2021-05-28 10:53:30 +0530

    [Shreya]Add:

--- Basic_Core_Program/Basic_Core_Program/Program.cs
+++ Basic_Core_Program/Basic_Core_Program/Program.cs
@@ -11,6 +11,23 @@ namespace Basic_Core_Program
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Core Program");
+            Console.WriteLine("Enter your choice between 1 to 10: ");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("1 For: Flip a Coin Head and Tail. ");
+            int selection = Convert.ToInt32(Console.ReadLine());
+            // user selection
+
+            switch (selection)
+            {
+                case 1:
+                    //FLIP COIN
+                    FlipCoinHeadOrTail FlipCoin = new FlipCoinHeadOrTail();
+                    FlipCoin.CheckHeadTailPercentage();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Selection.");
+                    break;
+            }
             Console.ReadLine();
         }
     }
